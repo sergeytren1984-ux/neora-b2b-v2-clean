b078f84ba8eeffafc57041336d514b01776f706e
sergeytren1984-ux <sergeytren1984@gmail.com>
2025-11-05 21:22:55 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: { colors: { brand: 'var(--brand)' } } },
-  plugins: []
+  theme: {
+    extend: {
+      colors: {
+        brand: '#C58A44' // твой “золотисто-медный É”
+      },
+      borderRadius: {
+        '2xl': '1.25rem'
+      }
+    },
+  },
+  plugins: [],
 }

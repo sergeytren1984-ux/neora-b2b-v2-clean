048100dcdcee2303be2785d10846339213352bec
sergeytren1984-ux <sergeytren1984@gmail.com>
2025-11-05 23:30:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// НИКАКОЙ кастомной base — оставь по умолчанию
 export default defineConfig({
   plugins: [react()]
-})
+});

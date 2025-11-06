7f385e8f51be05bfb0b2ca1c99a4469816511684
sergeytren1984-ux <sergeytren1984@gmail.com>
2025-11-06 14:05:05 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-// если нет файла styles.css — УДАЛИ эту строку ниже
-// import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>

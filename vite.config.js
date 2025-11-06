ac5c15d1fd5dac412dbde12e961feb953e59ca3c
sergeytren1984-ux <sergeytren1984@gmail.com>
2025-11-06 15:20:37 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
+  build: { outDir: "dist" }
 });
